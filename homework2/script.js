fa604acae96156fca638ca1cdce569ec98714b7b
MaxSumets <archreaver01@gmail.com>
2022-01-27 19:02:54 +0200

    created homework2 code

---
+++ homework2/script.js
@@ -0,0 +1,17 @@
+
+// 1
+let userAge = '23';
+let userName = 'Igor';
+let userSurname = 'Ivanov';
+
+// 2
+let newUser;
+let shoppingList;
+let isUserOnline;
+let workerSalary;
+let cursorCoordinates;
+
+// 3
+console.log('Hello world!')
+let onlineUsers = '35';
+console.log('Amount of online users is',onlineUsers);
